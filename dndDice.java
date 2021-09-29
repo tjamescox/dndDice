49c940ff1b82fa88d2f768b39202f65ff304f136
tjamescox <tjamescox95@gmail.com>
2021-09-29 10:20:19 -0400

    Updated to improve readability

--- dndDice.java
+++ dndDice.java
@@ -3,140 +3,45 @@ import java.util.Random;
 
 public class dndDice
 {
+    public static void rollDice(int selectedDice)
+    {
+        Random randomizer = new Random();
+        int answer = randomizer.nextInt(selectedDice) + 1;
+        System.out.print(answer);
+    }
+
     public static void main(String[] args)
     {
-        int[] dFour = {1, 2, 3, 4};
-        int[] dSix = {1, 2, 3, 4, 5, 6};
-        int[] dEight = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] dTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] dTwelve = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int[] dTwenty = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int[] diceOptions = {4, 6, 8, 10, 12, 20};
 
         Scanner keyboard = new Scanner(System.in);
-        Random r = new Random();
 
         char Yes = 'Y';
         char No = 'N';
         char Answer;
 
-        int subscript = 0;
-
-        int four = 4;
-        int six = 6;
-        int eight = 8;
-        int ten = 10;
-        int twelve = 12;
-        int twenty = 20;
         int diceRoll;
 
-        System.out.print("What dice should we roll?: d");
-        diceRoll = keyboard.nextInt();
-
-        //Need while loop for incorrect responses
-        // while (diceRoll != four)
-        // {
-        //     System.out.print("What dice should we roll?: d");
-        //     diceRoll = keyboard.nextInt();
-        // }
-
-        if (diceRoll == four)
-        {
-            subscript = r.nextInt(4);
-            System.out.println(dFour[subscript]);
-        }
-
-        else if (diceRoll == six)
-        {
-            subscript = r.nextInt(6);
-            System.out.println(dSix[subscript]);
-        }
-
-        else if (diceRoll == eight)
-        {
-            subscript = r.nextInt(8);
-            System.out.println(dEight[subscript]);
-        }
-
-        else if (diceRoll == ten)
-        {
-            subscript = r.nextInt(10);
-            System.out.println(dTen[subscript]);
-        }
-
-        else if (diceRoll == twelve)
-        {
-            subscript = r.nextInt(12);
-            System.out.println(dTwelve[subscript]);
-        }
-
-        else if (diceRoll == twenty)
-        {
-            subscript = r.nextInt(20);
-            System.out.println(dTwenty[subscript]);
-        }
-
-        else
-        {
-            System.out.println("invalid dice");
-        }
-
-        System.out.print("\nRoll again? (Y/N): ");
-        Answer = keyboard.next().charAt(0);
-
-        while (Answer != Yes && Answer != No)
-        {
-            System.out.println("invalid entry try again");
-            System.out.print("\nRoll again? (Y/N): ");
-            Answer = keyboard.next().charAt(0);
-        }
+        boolean validDice = false;
 
         //Begin
-        while (Answer == Yes)
-        {
+        do{
+            do {
             System.out.print("What dice should we roll?: d");
             diceRoll = keyboard.nextInt();
 
-        //Need while loop for incorrect responses
-        if (diceRoll == four)
-        {
-            subscript = r.nextInt(4);
-            System.out.println(dFour[subscript]);
-        }
+            for(int i = 0; i < diceOptions.length; i++) {
+                if(diceOptions [i] == diceRoll) {
+                    validDice = true;
+                    break;
+                }
+                else {
+                    validDice = false;
+                }
+            }
+        }while (validDice == false);
 
-        else if (diceRoll == six)
-        {
-            subscript = r.nextInt(6);
-            System.out.println(dSix[subscript]);
-        }
-
-        else if (diceRoll == eight)
-        {
-            subscript = r.nextInt(8);
-            System.out.println(dEight[subscript]);
-        }
-
-        else if (diceRoll == ten)
-        {
-            subscript = r.nextInt(10);
-            System.out.println(dTen[subscript]);
-        }
-
-        else if (diceRoll == twelve)
-        {
-            subscript = r.nextInt(12);
-            System.out.println(dTwelve[subscript]);
-        }
-
-        else if (diceRoll == twenty)
-        {
-            subscript = r.nextInt(20);
-            System.out.println(dTwenty[subscript]);
-        }
-
-        else
-        {
-            System.out.println("invalid dice");
-        }
+        rollDice(diceRoll);
 
         System.out.print("\nRoll again? (Y/N): ");
         Answer = keyboard.next().charAt(0);
@@ -147,7 +52,7 @@ public class dndDice
             System.out.print("\nRoll again? (Y/N): ");
             Answer = keyboard.next().charAt(0);
         }
-        }
+        }while (Answer == Yes);
         
 
     }
